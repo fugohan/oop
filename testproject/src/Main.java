3bd4dbb143e17d5b3d49e91935d8799c71120bb8
fugohan <fugohan@onlinetu.tk>
2019-04-11 15:43:22 +0200

    Test Project is working and now better

--- testproject/src/Main.java
+++ testproject/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Ru schafft die ET1 Prüfung heute!");
     }
 }
